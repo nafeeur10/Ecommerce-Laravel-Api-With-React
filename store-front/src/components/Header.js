5cf1fdcf4cd59b8041425358a4b361fe85ee39fd
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2021-12-18 00:10:37 +0600

    Login page and Home page separated

---
+++ store-front/src/components/Header.js
@@ -0,0 +1,130 @@
+import { MenuIcon, SearchIcon, ShoppingBagIcon } from '@heroicons/react/outline'
+import React, { useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Dialog, Transition } from '@headlessui/react'
+import { XIcon } from '@heroicons/react/outline'
+
+const Header = () => {
+    const [open, setOpen] = useState(false)
+    return (
+        <div className="bg-white">
+            <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
+            <Transition.Child
+                as={Fragment}
+                enter="transition-opacity ease-linear duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity ease-linear duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+            >
+                <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+                <div className="px-4 pt-5 pb-2 flex">
+                    <button
+                    type="button"
+                    className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                    onClick={() => setOpen(false)}
+                    >
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+
+                <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                    <div className="flow-root">
+                    <Link to="/login">
+                    <a className="-m-2 p-2 block font-medium text-gray-900">
+                        Sign in
+                    </a>
+                    </Link>
+                    </div>
+                    <div className="flow-root">
+                    <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
+                        Create account
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </Transition.Child>
+            </Dialog>
+            </Transition.Root>
+            <header className="relative bg-white">
+                <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="border-b border-gray-200">
+                    <div className="h-16 flex items-center">
+                    <button
+                        type="button"
+                        className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+                        onClick={() => setOpen(true)}
+                    >
+                        <span className="sr-only">Open menu</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+
+                    {/* Logo */}
+                    <div className="ml-4 flex lg:ml-0">
+                        <Link to="/">
+                            <a className="flex">
+                                <span className="sr-only">weDevs Dokan</span>
+                                <img src="https://img.icons8.com/color/48/000000/stall.png"/>
+                            </a>
+                        </Link>
+                    </div>
+
+                
+
+                    <div className="ml-auto flex items-center">
+                        <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                        <Link to="/login">
+                            <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                                Log in
+                            </a>
+                        </Link>
+                        <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                        <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                            Create account
+                        </a>
+                        </div>
+
+                        {/* Search */}
+                        <div className="flex lg:ml-6">
+                        <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Search</span>
+                            <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                        </a>
+                        </div>
+
+                        {/* Cart */}
+                        <div className="ml-4 flow-root lg:ml-6">
+                        <a href="#" className="group -m-2 p-2 flex items-center">
+                            <ShoppingBagIcon
+                            className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                            />
+                            <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                            <span className="sr-only">items in cart, view bag</span>
+                        </a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header
---
+++ store-front/src/pages/Home/Home.js
@@ -0,0 +1,245 @@
+
+
+
+  
+  const products = [
+    {
+      id: 1,
+      name: 'Earthen Bottle',
+      href: '#',
+      price: '$48',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 2,
+      name: 'Nomad Tumbler',
+      href: '#',
+      price: '$35',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      name: 'Focus Paper Refill',
+      href: '#',
+      price: '$89',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      name: 'Machined Mechanical Pencil',
+      href: '#',
+      price: '$35',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    // More products...
+  ]
+  
+  const features = [
+    {
+      name: 'FREE RETURNS',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: "https://img.icons8.com/color/48/000000/return-purchase.png",
+    },
+    {
+      name: 'NO HIDDEN FEES',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: "https://img.icons8.com/color/48/000000/no-hidden-fee.png",
+    },
+    {
+      name: 'SAME DAY DELIVERY',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: "https://img.icons8.com/color/48/000000/time-card.png",
+    },
+    {
+      name: 'MOBILE NOTIFICATION',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: "https://img.icons8.com/color/48/000000/push-notifications.png",
+    },
+  ]
+  
+  const newFeatures = [
+    { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+    { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+    { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+    { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+    { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+    { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+  ]
+  
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
+
+const Home = () => {
+    return (
+        <div className="bg-white">
+      {/* Mobile menu */}
+    
+
+      
+
+      <div className="container mx-auto py-9 md:py-12 px-4 lg:px-8 lg:max-w-7xl max-w-2xl">
+            <div className="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8">
+                <div className="flex flex-col md:flex-row items-strech justify-between bg-gray-50 py-6 px-6 md:py-12 lg:px-12 md:w-6/12 lg:w-6/12 xl:w-6/12 2xl:w-6/12">
+                    <div className="flex flex-col justify-center md:w-1/2">
+                        <h1 className="text-3xl lg:text-4xl font-semibold text-gray-800">Best Deal</h1>
+                        <p className="text-base lg:text-xl text-gray-800 mt-2">
+                            Save upto <span className="font-bold">50%</span>
+                        </p>
+                    </div>
+                    <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
+                        <img src="https://i.ibb.co/J2BtZdg/Rectangle-56-1.png" alt="" />
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row items-strech justify-between bg-gray-50 py-6 px-6 md:py-12 lg:px-12 md:w-6/12 lg:w-6/12 xl:w-6/12 2xl:w-6/12">
+                    <div className="flex flex-col justify-center md:w-1/2">
+                        <h1 className="text-3xl lg:text-4xl font-semibold text-gray-800">Best Deal</h1>
+                        <p className="text-base lg:text-xl text-gray-800 mt-2">
+                            Save upto <span className="font-bold">50%</span>
+                        </p>
+                    </div>
+                    <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
+                        <img src="https://i.ibb.co/rGfP7mp/Rectangle-59-1.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      
+      <div className="max-w-2xl mx-auto lg:max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div>
+              <a key={product.id} href={product.href} className="group">
+                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="w-full h-full object-center object-cover group-hover:opacity-75"
+                  />
+                </div>
+
+                <div className="flex">
+                  <div>
+                    <h3 className="mt-3 text-md font-medium text-gray-900">{product.name}</h3>
+                  </div>
+                  <div className="ml-auto">
+                    <p className="mt-3 text-md font-medium text-gray-900">{product.price}</p>
+                  </div>
+                </div>
+              </a>
+              <button
+                className="mt-3 w-full border bg-gray-200 py-1.5 px-3 flex items-center justify-center text-base font-medium text-black"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" />
+                </svg>
+                <span>
+                Add to bag
+                </span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    
+      <div className="py-12 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Why we BEST</h2>
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Do you know why people like us?
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative">
+                  <dt>
+                    <div className="absolute flex items-center justify-center rounded border text-white">
+                      <img src={feature.icon} className="p-2" />
+                    </div>
+                    <p className="ml-20 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                  </dt>
+                  <dd className="mt-2 ml-20 text-base text-gray-500">{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      
+
+      <div className="max-w-2xl mx-auto lg:max-w-7xl py-3 px-4 sm:px-6 lg:px-8">
+            <div className=" w-full  relative flex items-center justify-center">
+                <div className="bg-gray-800 bg-opacity-80 md:my-16 lg:py-16 py-10 w-full flex flex-col items-center justify-center relative z-40">
+                    <h1 className="text-4xl font-semibold leading-9 text-white text-center">Don’t miss out!</h1>
+                    <p className="text-base leading-normal text-center text-white mt-6">
+                        Subscribe to your newsletter to stay in the loop. Our newsletter is sent once in <br />a week on every friday so subscribe to get latest news and updates.
+                    </p>
+                    <div className="sm:border border-white flex-col sm:flex-row  flex items-center lg:w-5/12 w-full mt-12 space-y-4 sm:space-y-0">
+                        <input className="border border-white sm:border-transparent text-base w-full font-medium leading-none text-white p-4 focus:outline-none bg-transparent placeholder-white" placeholder="Email Address" />
+                        <button className="focus:outline-none focus:ring-offset-2 focus:ring border border-white sm:border-transparent w-full sm:w-auto bg-white py-4 px-6 hover:bg-opacity-75">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+      </div>
+
+      <div className="max-w-2xl mx-auto py-8 px-4 grid items-center grid-cols-1 gap-y-8 gap-x-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+        <div>
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+          <p className="mt-4 text-gray-500">
+            The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
+            steel divider separates active cards from new ones, or can be used to archive important task lists.
+          </p>
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {newFeatures.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{feature.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+            alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+            className="bg-gray-100 rounded-lg"
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
+            alt="Top down view of walnut card tray with embedded magnets and card groove."
+            className="bg-gray-100 rounded-lg"
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
+            alt="Side of walnut card tray with card groove and recessed card area."
+            className="bg-gray-100 rounded-lg"
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
+            alt="Walnut card tray filled with cards and card angled in dedicated groove."
+            className="bg-gray-100 rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Home
